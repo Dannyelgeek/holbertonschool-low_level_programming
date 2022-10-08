50c0bf74bb68bbac2656ba7fa3d619765966108b
dannyelgeek <daot1993@gmail.com>
2022-10-07 19:39:17 -0700

    C Hello, World task 6

---
+++ hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <studio>
+
+/**
+ * long - prints the size of the types
+ * Retunr: 0 if exited properly, non-zero otherwise
+ */
+long(void)
+{
+	printf("Size of a char: 1 byte(s)\n")
+	printf("Size of an int: 4 byte(s)\n")
+	printf("Size of a long int: 4 byte(s)\n")
+	printf("Size of a long long int: 8 byte(s)\n")
+	printf("Size of a float: 4 byte(s)\n")
+	return (0);
+}
