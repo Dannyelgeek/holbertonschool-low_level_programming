ad17eacf9d6c76277b1a023674c7d8802547acb0
dannyelgeek <daot1993@gmail.com>
2022-10-07 18:43:57 -0700

    C Hello, World task 4

---
+++ hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/*
+ * C program for Hello, World task 4
+ */
+int main(int argc, char* argv[])
+{
+	int str[] = "\"Programming is like building a multilingual puzzle";
+	puts(str)
+	return 0;
+}/*end of the exercise*/
