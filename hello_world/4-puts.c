de02b6c28d2fe290ff18107f37e892b1dfb49b06
dannyelgeek <daot1993@gmail.com>
2022-10-07 19:05:11 -0700

    C Hello, World task 4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 
 /**
- * C program for Hello, World task four
+ * main: prints the phrase \"Programming is like building a multilingual puzzle
+ * return: ends with the program
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 }
