1d20081252a9e7ca761b7c5c590624573937b5d0
Dannyelgeek <daot1993@gmail.com>
2022-10-10 17:53:37 -0500

    if, else and while task 1

---
+++ variables_if_else_while/1-last_digit.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+* main - print the last digit of the number stored in the variable n.
+* Return: 0 if exited promerly, non-zero otherwise.
+*/
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+}
