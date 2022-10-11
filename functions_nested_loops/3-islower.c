9c30b5d9dbc8d4cd2acb91cfb234b81f4b0c1667
Dannyelgeek <daot1993@gmail.com>
2022-10-11 13:42:43 -0500

    functions, nested loops task 3

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* _islower -  prints the alphabet x10 with _putchar.
+* @c: character to check.
+* Return: 0 if exited correctly, non-zero otherwise.
+*/
+
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	return(1);
+	else
+	return(0);
+}
