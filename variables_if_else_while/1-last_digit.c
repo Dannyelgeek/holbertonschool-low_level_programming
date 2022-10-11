54662c415cc084ca55b87254dd36e463ee1b6222
Dannyelgeek <daot1993@gmail.com>
2022-10-10 19:07:22 -0500

    if, else and while task 1

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -15,15 +15,15 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	if (n % 10  > 5)
 	{
-		printf("%i Lasta digit of\n", n);
+		printf("Lasta digit of\n") n;
 	}
 	else if (n % 10 == 0)
 	{
-		printf("%i Lastb digit of\n", n);
+		printf("Lastb digit of\n") n;
 	}
 	else
 	{
-		printf("%i Lastc digit of\n", n);
+		printf("Lastc digit of\n") n;
 }
 return (0);
 }
