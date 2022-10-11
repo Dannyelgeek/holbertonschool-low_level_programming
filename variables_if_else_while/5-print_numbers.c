69dd48fc68be4b741f4dccc84e52287395af21c9
Dannyelgeek <daot1993@gmail.com>
2022-10-10 22:02:30 -0500

    if, else and while task 5

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+* main -  prints all single digit numbers of base 10 starting from 0.
+* Return: 0 if exited correctly, non-zero otherwise.
+*/
+
+long main(void)
+{
+	long l = 0123456789
+	printf("%li\n", l);
+	return (0);
+}
