ce7eae4f4377209f6fb532906a754118d8393b4f
Dannyelgeek <daot1993@gmail.com>
2022-10-11 11:13:12 -0500

    functions, nested loops task 0

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -6,6 +6,13 @@
  */
 int main(void)
 {
-	puts("_putchar");
+	_putchar("_");
+	_putchar("p");
+	_putchar("u");
+	_putchar("t");
+	_putchar("c");
+	_putchar("h");
+	_putchar("a");
+	_putchar("r");
 	return (0);
 }
