63fb62d8a39b7fadafb4af2df030c65c6811c96f
Dannyelgeek <daot1993@gmail.com>
2022-10-10 22:48:04 -0500

    if, else and while task 8

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -8,9 +8,13 @@
 int main(void)
 {
 	int n;
+	char l;
 
-	for (n = '0'; n <= 'f'; n++)
+	for (n = '0'; n <= '9'; n++)
 	putchar(n);
+
+	for (l = 'a'; l <= 'f'; l++)
+	putchar(l);
 
 	putchar('\n');
 	return (0);
