30e54e464a66c83e7b3defb9fa38a4b4d98e0436
Dannyelgeek <daot1993@gmail.com>
2022-10-11 15:27:23 -0500

    functions, nested loops task 6

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* _abs -  prints the sign of a number.
+* @int: character to check.
+* Return: 0 if exited correctly, non-zero otherwise.
+*/
+
+int _abs(int n)
+{
+if (n >= 0)
+return (n);
+else
+return (-n);
+}
