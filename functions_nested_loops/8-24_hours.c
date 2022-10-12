1cf3cdfacef2cd6e20d15cbd4ff747adf24e3ba7
Dannyelgeek <daot1993@gmail.com>
2022-10-11 19:18:16 -0500

    functions, nested loops task 8

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+* jack_bauer -  hours and minutes with _putchar.
+* Return: 0 if exited correctly, non-zero otherwise.
+*/
+
+void jack_bauer(void)
+{
+	int hour, minute;
+
+	for (hour = 0; hour <= 23; hour++)
+	{
+		for (minute = 0; minute <= 59; minute++)
+			{
+			_putchar((hour / 10) + '0');
+			_putchar((hour % 10) + '0');
+			_putchar(':');
+			_putchar((minute / 10) + '0');
+			_putchar((minute % 10) + '0');
+			_putchar('\n');
+		}
+	}
+}
