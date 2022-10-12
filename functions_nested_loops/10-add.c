d6295038b0da87afa04ca8ad3595cf9a7065bf01
Dannyelgeek <daot1993@gmail.com>
2022-10-11 20:05:36 -0500

    functions, nested loops task 10

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+* add - adds two integers and returns the result.
+* @n1: frist integer to add.
+* @n2: second integer to add.
+* Return: 0 if exited correctly, non-zero otherwise.
+*/
+
+int add(int n1, int n2)
+{
+	return (n1 + n2);
+}
