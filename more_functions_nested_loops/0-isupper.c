0c31efcc665a37cc3844037d0dd58a46d7ee6c94
dannyelgeek <daot1993@gmail.com>
2022-10-13 11:32:57 -0700

    more f,n,l task 0

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* _isupper -  prints if the character is uppercase.
+* @c: character to check.
+* Return: 0 if exited correctly, non-zero otherwise.
+*/
+
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}
+
