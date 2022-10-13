b1c7a1666930e536227a5521c76b96993c2e1798
Dannyelgeek <daot1993@gmail.com>
2022-10-13 16:17:24 -0500

    more f,n,l task 2

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+* mul - adds two integers and returns the result.
+* @a: frist factor.
+* @b: second factor.
+* Return: 0 if exited correctly, non-zero otherwise.
+*/
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
