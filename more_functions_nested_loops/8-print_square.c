abdf44caffa2802c8bd4a956501904e369b8f4fe
Dannyelgeek <daot1993@gmail.com>
2022-10-14 13:51:26 -0500

    more f,n,l task 8

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -1,22 +1,22 @@
 #include "main.h"
 
 /**
-* rint_diagonal - draws a diagonal line in the terminal.
-* @n: times to print '\'
+* print_square - draws a square the terminal.
+* @size: times to print '\'
 * Return: 0 if exited correctly, non-zero otherwise.
 */
 
 void print_square(int size)
 {
 	int h;
-    int r;
+	int r;
 
-	for(h = 1; h <= size; h++)
+	for (h = 1; h <= size; h++)
 	{
-        for(r = h - 1; r <= size - 2; r++)
-        {
-            _putchar('#');
-        }
+		for (r = 1; r <= size - 1; r++)
+		{
+			_putchar('#');
+		}
 		_putchar('#');
 		_putchar('\n');
 	}
