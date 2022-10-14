833fadfc8eb97ee218774b6a825e5b42198020a1
Dannyelgeek <daot1993@gmail.com>
2022-10-14 15:12:01 -0500

    more f,n,l task 9

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -1,36 +1,40 @@
 #include <stdio.h>
 
 /**
-* main - draws a square the terminal.
+* main - Fizz Buzz excersise.
 * Return: 0 if exited correctly, non-zero otherwise.
 */
 
-void main(void)
+int main(void)
 {
-int c;
+	int c;
 
-for (c = 1; c <= 100; c++)
-{
-	if ((c % 3) == 0 && (c % 5) == 0)
+	for (c = 1; c <= 100; c++)
 	{
-		printf("FizzBuzz ");
+		if ((c % 3) == 0 && (c % 5) == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if ((c % 3) == 0)
+		{
+			printf("Fizz ");
+		}
+		else if ((c % 5) == 0)
+		{
+			if (c == 100)
+			{
+				printf("Buzz");
+			}
+			else
+			{
+				printf("Buzz ");
+			}	
+		}
+		else
+		{
+			printf("%i ", c);
+		}
 	}
-	else if ((c % 3) == 0)
-	{
-		printf("Fizz ");
-	}
-	else if ((c % 5) == 0)
-	{
-		printf("Buzz ");
-	}
-	else
-	{
-		printf("%i ", c);
-	}
-}
-while (c == 100)
-{
-	printf("buzz\n");
-}
 	printf("\n");
+	return (0);
 }
