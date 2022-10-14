dd6f69555beb841778a813c5df875d72aa814e1a
Dannyelgeek <daot1993@gmail.com>
2022-10-14 15:59:18 -0500

    more f,n,l task 10

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -1,27 +1,31 @@
 #include "main.h"
 
 /**
-* print_square - draws a square the terminal.
+* print_triangle - draws a triangle the terminal.
 * @size: times to print '#'
 * Return: 0 if exited correctly, non-zero otherwise.
 */
 
-void print_square(int size)
+void print_triangle(int size)
 {
 	int h;
 	int r;
 
 	for (h = 1; h <= size; h++)
 	{
-		for (r = 1; r <= size - 1; r++)
+		for (r = 1; r <= size; r++)
 		{
-			_putchar('#');
+			if ((size - r) >= h)
+				_putchar(' ');
+			else
+			{
+				_putchar('#');
+			}
 		}
-		_putchar('#');
 		_putchar('\n');
-	}
 	if (size <= 0)
 	{
 		_putchar('\n');
 	}
+}
 }
