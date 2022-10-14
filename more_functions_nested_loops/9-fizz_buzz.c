b500d5e93657e777d81686cf9a54ed1020631f3c
Dannyelgeek <daot1993@gmail.com>
2022-10-14 14:26:49 -0500

    more f,n,l task 9

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+* main - draws a square the terminal.
+* Return: 0 if exited correctly, non-zero otherwise.
+*/
+
+void main(void)
+{
+int c;
+
+for (c = 1; c <= 100; c++)
+{
+	if ((c % 3) == 0)
+	{
+		printf("Fizz ");
+	}
+	else if ((c % 5) == 0)
+	{
+		printf("Buzz ");
+	}
+	else
+	{
+		printf("%i ", c);
+	}
+}
+printf("\n");
+}
