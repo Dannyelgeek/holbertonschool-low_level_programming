096aa10f60a3b747cc9d40ec2c185678128b9a5f
Dannyelgeek <daot1993@gmail.com>
2022-10-13 20:38:41 -0500

    more f,n,l task 5

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+* more_numbers - print the numbers from 0 to 14 x10.
+* Return: 0 if exited correctly, non-zero otherwise.
+*/
+
+void more_numbers(void)
+{
+int c;
+int n;
+int d;
+
+for (c = 0; c < 10; c++)
+{
+	for (n = 0; n <= 1; n++)
+	{
+		for(d = 0; d <= 9; d++)
+		{
+			if (n != 0)
+			{
+				if (n > 0 && d <= 4)
+				_putchar(n + '0');
+			}
+			if (n == 0 || d < 5)
+			_putchar(d + '0');
+		}
+	}
+	_putchar('\n');
+}
+}
