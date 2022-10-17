e3dd94012404edaf72318c7c1f1428e61df7859f
Dannyelgeek <daot1993@gmail.com>
2022-10-17 12:28:45 -0500

    pointers task 1

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+* reset_to_98 -  updates the value it points to to 98.
+* @n: character to check.
+* Return: 0 if exited correctly, non-zero otherwise.
+*/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
