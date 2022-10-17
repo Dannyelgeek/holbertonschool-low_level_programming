a2615503929c89dc8063b60f0bafa17914c994ea
Dannyelgeek <daot1993@gmail.com>
2022-10-17 16:46:10 -0500

    pointers task 3

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* _puts -  prints a string.
+* @str: character to check.
+* Return: 0 if exited correctly, non-zero otherwise.
+*/
+
+void _puts(char *str)
+{
+	int p = '\0';
+
+	while (str[p] != '\0')
+	{
+		_putchar(str[p]);
+		p++;
+	}
+	_putchar('\n');
+}
+
