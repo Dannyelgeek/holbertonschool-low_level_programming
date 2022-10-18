b444c76ebe4dad3fbf46a658f1645a1461bca797
Dannyelgeek <daot1993@gmail.com>
2022-10-18 15:51:28 -0500

    pointers task 5

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+* rev_string -  prints a string in reverse.
+* @s: character to check.
+* Return: 0 if exited correctly, non-zero otherwise.
+*/
+
+void rev_string(char *s)
+{
+	int p = 0, r = 0;
+	char tmp;
+
+	while (s[p] != '\0')
+	{
+		p += 1;
+	}
+	p -= 1;
+	
+	for (r = 0; r < p; r++)
+	{
+		tmp = s[r];
+		s[r] = s[p];
+		s[p] = tmp;
+		p--;
+	}
+}
