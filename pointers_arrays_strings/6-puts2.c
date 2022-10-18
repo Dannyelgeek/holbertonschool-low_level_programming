fba13c92a949909cc650f8ad427bded7a246c7a2
Dannyelgeek <daot1993@gmail.com>
2022-10-18 16:48:13 -0500

    pointers task 6

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
-*  -  prints a string in reverse.
+* puts2-  prints every other character of a string, 
+* starting with the first character, followed by a new line.
 * @str: character to check.
 * Return: 0 if exited correctly, non-zero otherwise.
 */
