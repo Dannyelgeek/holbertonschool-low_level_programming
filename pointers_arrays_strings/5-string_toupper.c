1da1fa89947b5a8510c089c0c556b092f8c3ae40
Dannyelgeek <daot1993@gmail.com>
2022-10-19 18:16:23 -0500

    more pointers task 5

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * string_toupper - transform the lowercase in uppercase:
+ * @scr: pointer of the array.
+ * Return: scr
+ */
+
+char *string_toupper(char *scr)
+{
+	int a = 0, u;
+
+	while (scr[a] != '\0')
+	{
+		a += 1;
+	}
+
+	for (u = 0; u < a; u++)
+	{
+		if (scr[u] >= 'a' && scr[u] <= 'z')
+		{
+			scr[u] = scr[u] - 32;
+		}
+	}
+	return(scr);
+}
+
