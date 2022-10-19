1df1ccc6f90ff2af3c1675cb7f045eadd556be5c
Dannyelgeek <daot1993@gmail.com>
2022-10-19 12:42:05 -0500

    more pointers task 2

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+*  *_strncat -  prints a concatenate string.
+* @src: character to check.
+* @dest: sttring to concatenate.
+* @n: array to limit.
+* Return: dest if exited correctly, non-zero otherwise.
+*/
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int cop;
+
+	for (cop = 0; cop < n && src [cop] != '\0'; cop++)
+		dest[cop] = src[cop];
+	for (; cop < n; cop++)
+		dest[cop] = '\0';
+
+	return (dest);
+}
