3f167bbafaaf4b6ead441a61ac1081e994eb4058
Dannyelgeek <daot1993@gmail.com>
2022-10-21 12:56:32 -0500

    even more pointers task 5

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * *_strstr - locates a substring.
+ * @haystack: string to be read.
+ * @needle: substring to be find.
+ * Return: '\0' if exited correctly.
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int fnd;
+
+	if (*needle == 0)
+		return (haystack);
+
+	while (*haystack)
+	{
+		fnd = 0;
+
+		if (haystack[fnd] == needle[fnd])
+		{
+			do
+			{
+				if (needle[fnd + 1] == '\0')
+					return (haystack);
+				fnd++;
+			}
+		while (haystack[fnd] == needle[fnd]);
+		}
+		haystack++;
+	}
+	return (0);
+}
+
