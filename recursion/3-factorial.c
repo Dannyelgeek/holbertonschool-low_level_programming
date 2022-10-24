c04a67bb26f7cc93c0f9c2352672b2250b5b9d78
Dannyelgeek <daot1993@gmail.com>
2022-10-24 17:44:38 -0500

    recursion task 3

---
+++ recursion/3-factorial.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * factorial - returns the factorial of the given number.
+ * @n: given number.
+ * Return: 1 if condition is right.
+ */
+
+int factorial(int n)
+{
+	if (n == 0)
+		return (1);
+
+	else if (n < 0)
+		return(-1);
+
+	else
+		return (n * factorial(n - 1));
+}
+
