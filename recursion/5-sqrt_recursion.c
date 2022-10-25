94bf7c5adeb8b3e7aa82d0b29e9654d0df7c1f2b
Dannyelgeek <daot1993@gmail.com>
2022-10-25 17:17:22 -0500

    recursion task 5

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -1,8 +1,9 @@
 #include "main.h"
 
 /**
- * _sqrt_recursion - returns the natural 
+ * _sqrt_recursion - returns the natural
  * square root of a number.
+ * root - auxiliar function.
  * @n: number to root.
  * Return: 1 if exited correctly.
  */
@@ -14,7 +15,7 @@ int _sqrt_recursion(int n)
 	if (n < 0)
 		return (-1);
 	else
-		return (root (n, r1));
+		return (root(n, r1));
 }
 
 int root(int n, int r1)
@@ -22,8 +23,8 @@ int root(int n, int r1)
 	if (r1 * r1 == n)
 		return (r1);
 
-	else if (r1 * r1 < n)	
-		return (root (n, r1 + 1));
+	else if (r1 * r1 < n)
+		return (root(n, r1 + 1));
 
 	else
 		return (-1);
