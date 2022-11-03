5c7a46d0c6d190ff18642a11224f1953f7db64b6
Dannyelgeek <daot1993@gmail.com>
2022-11-03 11:02:18 -0500

    more malloc task 0

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -4,13 +4,14 @@
 /**
  * malloc_checked - allocates memory using malloc.
  * @b: size of the memory.
+ * Return: point if exited correctly.
 */
 
 void *malloc_checked(unsigned int b)
 {
 	char *point;
 
-	point = malloc(sizeof(int) * b);
+	point = malloc(sizeof(char) * b);
 
 	if (point == NULL)
 		exit(98);
