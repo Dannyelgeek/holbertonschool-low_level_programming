0bb54972a23825625a28c8026fa2352bddf3616a
Dannyelgeek <daot1993@gmail.com>
2022-11-08 14:49:26 -0500

    structures task 5

--- structures_typedef/5-free_dog.c
+++ structures_typedef/5-free_dog.c
@@ -8,11 +8,10 @@
 
 void free_dog(dog_t *d)
 {
+	free(d);
 	if (d->name != NULL && d != NULL)
 		free(d->name);
 	if (d->owner != NULL && d != NULL)
 		free(d->owner);
-	if (d != NULL)
-		free(d);
 }
 
