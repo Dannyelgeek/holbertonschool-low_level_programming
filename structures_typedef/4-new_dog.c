1534226bce825329f8194ff9fe3282440f1a6ae2
Dannyelgeek <daot1993@gmail.com>
2022-11-08 12:55:44 -0500

    structures task 4

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -7,13 +7,23 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *lucky;
+	int lenname = 0, lenowner = 0, copname, copowner;
+
+	while (name[lenname] != '\0')
+		lenname++;
+	while (owner[lenowner] != '\0')
+		lenowner++;
 
 	lucky = malloc(sizeof(dog_t));
 	if (lucky == NULL)
 		return (NULL);
-	lucky->name = owner;
+	lucky->name = malloc(lenname + 1);
+	lucky->owner = malloc(lenowner + 1);
+	for (copname = 0; copname <= lenname; copname++)
+		lucky->name[copname] = name[copname];
+	for (copowner = 0; copowner <= lenowner; copowner++)
+		lucky->owner[copowner] = owner[copowner];
 	lucky->age = age;
-	lucky->owner = name;
 	return (lucky);
 }
 
