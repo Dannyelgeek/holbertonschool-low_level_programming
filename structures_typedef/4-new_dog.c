df126677c4c1615361a29f02411b97e615822dfd
Dannyelgeek <daot1993@gmail.com>
2022-11-08 12:11:56 -0500

    structures task 4

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+*/
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *lucky;
+
+	lucky = malloc(sizeof(dog_t));
+	if (lucky == NULL)
+		return (NULL);
+	lucky->name = name;
+	lucky->age = age;
+	lucky->owner = owner;
+	return (lucky);
+}
+
