85bf69553a896103affeac82aee7d8977ea505de
Dannyelgeek <daot1993@gmail.com>
2022-11-30 08:34:33 -0500

    s_linked list task 0

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -3,11 +3,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * struct list_s - main node of the list.
+*/
 typedef struct list_s
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
+	char *str;
+	unsigned int len;
+	struct list_s *next;
 } list_t;
 
 size_t print_list(const list_t *h);
