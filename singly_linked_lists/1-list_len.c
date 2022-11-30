a7b058c6ec61b6463f34ed509368bc6d76483aa0
Dannyelgeek <daot1993@gmail.com>
2022-11-30 08:43:53 -0500

    s_linked list task 1

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -1,6 +1,9 @@
 #include "lists.h"
 
 /**
+ * list_len - returns the number of elements in a linked list.
+ * @h: pointer to the header of the list.
+ * Return: count if exited correctly.
 */
 
 size_t list_len(const list_t *h)
