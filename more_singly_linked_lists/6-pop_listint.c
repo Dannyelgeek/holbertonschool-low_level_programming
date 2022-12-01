9ff269315548d984b349001cd3a158d0f12ca306
Dannyelgeek <daot1993@gmail.com>
2022-12-01 12:41:25 -0500

    more s_linked list task 6

---
+++ more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * pop_listint - deletes the head node of a list.
+ * @head: pointer to the header of the list.
+ * Return: ind if exited correctly.
+*/
+
+int pop_listint(listint_t **head)
+{
+    int ind;
+    listint_t *currnt, *tmp;
+
+    if (!head)
+        return (0);
+    tmp = currnt = *head;
+    if (*head)
+    {
+        ind = currnt->n;
+        *head = currnt->next;
+        free(tmp);
+    }
+    else
+        ind = 0;
+    return (ind);
+}
