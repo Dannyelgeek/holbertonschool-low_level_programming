1911050f5ab93a5fb72eb5ccd92fa8428eea04ef
Dannyelgeek <daot1993@gmail.com>
2022-12-01 12:31:07 -0500

    more s_linked list task 5

---
+++ more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a list.
+ * @head: pointer to the header of th list
+*/
+
+void free_listint2(listint_t **head)
+{
+    listint_t *tmp ,*currnt;
+
+    if (!head)
+        return;
+    currnt = *head;
+    while (currnt)
+    {
+        tmp = currnt;
+        currnt = currnt->next;
+        free(tmp);
+    }
+    *head = NULL;
+}
