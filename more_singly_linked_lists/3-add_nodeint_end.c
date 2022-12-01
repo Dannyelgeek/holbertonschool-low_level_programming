e81bee2adc76b3d82524e27a1ba4f66ced3a505f
Dannyelgeek <daot1993@gmail.com>
2022-12-01 12:19:11 -0500

    more s_linked list task 3

--- more_singly_linked_lists/3-add_nodeint_end.c
+++ more_singly_linked_lists/3-add_nodeint_end.c
@@ -1,10 +1,10 @@
 #include "lists.h"
 
 /**
- * add_nodeint_end - 
- * @head:
- * @n:
- * Return:
+ * add_nodeint_end - adds a new node at the end of a list.
+ * @head: pointer to the header of the list
+ * @n: num on the list
+ * Return: head if exited correctly
 */
 
 listint_t *add_nodeint_end(listint_t **head, const int n)
