754990314636d645f8dcc8274012150a7eb6ae6e
Dannyelgeek <daot1993@gmail.com>
2022-12-01 12:58:49 -0500

    more s_linked list task 7

---
+++ more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index - returns the nth node of a list.
+ * @head: pointer to the header of the list.
+ * @index: hich node to stop at.
+ * Return: n if exited correctly.
+*/
+
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+    unsigned int n = 0;
+
+    while (n < index)
+    {
+        if (!head->next)
+            return (NULL);
+        head = head->next;
+        n += 1;
+    }
+    return (head);
+}
