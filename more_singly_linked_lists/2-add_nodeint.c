2114aa5572c9a7a3b61d548fbc145c53815b371b
Dannyelgeek <daot1993@gmail.com>
2022-12-01 12:01:22 -0500

    more s_linked list task 2

---
+++ more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+
+/**
+ * add_nodeint - adds a new node at the beginning of a list.
+ * @head: pionter of the header of th lists
+ * @n: num on the lists
+ * Return:
+*/
+
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *new_nod;
+
+	new_nod = (listint_t *)malloc(sizeof(listint_t));
+	if(!new_nod)
+		return (NULL);
+
+	new_nod->n = n;
+	new_nod->next = NULL;
+
+	if (!head)
+		head = &new_nod;
+	if (!(*head))
+		*head = new_nod;
+	else
+	{
+		new_nod->next = *head;
+		*head = new_nod;
+	}
+	return (new_nod);
+}
