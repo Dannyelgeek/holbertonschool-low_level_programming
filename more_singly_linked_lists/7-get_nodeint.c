9454ea07f3f8986bfd1039cabd5f6d1dc7cec17b
Dannyelgeek <daot1993@gmail.com>
2022-12-01 12:59:51 -0500

    more s_linked list task 7

--- more_singly_linked_lists/7-get_nodeint.c
+++ more_singly_linked_lists/7-get_nodeint.c
@@ -9,14 +9,14 @@
 
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-    unsigned int n = 0;
+	unsigned int n = 0;
 
-    while (n < index)
-    {
-        if (!head->next)
-            return (NULL);
-        head = head->next;
-        n += 1;
-    }
-    return (head);
+	while (n < index)
+	{
+		if (!head->next)
+			return (NULL);
+		head = head->next;
+		n += 1;
+	}
+	return (head);
 }
