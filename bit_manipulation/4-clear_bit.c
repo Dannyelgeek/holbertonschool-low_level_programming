1fbc263f8f3aca68f2710320644786a465080a52
Dannyelgeek <daot1993@gmail.com>
2022-12-02 18:52:46 -0500

    binary manipulation task 4

--- bit_manipulation/4-clear_bit.c
+++ bit_manipulation/4-clear_bit.c
@@ -4,6 +4,7 @@
  * clear_bit - sets the value of a bit to 0 at a given index.
  * @n: pointer to the bit.
  * @index: the index, starting from 0 of the bit you want to set.
+ * Return: 1 if extited correctly.
 */
 
 int clear_bit(unsigned long int *n, unsigned int index)
