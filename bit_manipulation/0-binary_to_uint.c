a695994d34a7afddcf450410633a99dfc37b42d1
Dannyelgeek <daot1993@gmail.com>
2022-12-02 13:27:34 -0500

    binary manipulation task 1

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -1,23 +1,25 @@
 #include "main.h"
 
 /**
- * binary_to_uint - 
+ * binary_to_uint -  converts a binary number to an unsigned int.
+ * @b: pointer to a string of 0 and 1 chars.
+ * Return: new_int if exited correctly.
 */
 
 unsigned int binary_to_uint(const char *b)
 {
-    int bin_len = 0, result = 1, new_int = 0;
+	int bin_len = 0, result = 1, new_int = 0;
 
-    if (!b)
-        return (0);
-    for (; b[bin_len]; bin_len++)
-        ;
-    for (bin_len -= 1; bin_len >= 0; bin_len--)
-    {
-        if (b[bin_len] != '0' && b[bin_len] != '1')
-            return (0);
-        new_int += result * (b[bin_len] - '0');
-        result *= 2;
-    }
-    return (new_int);
+	if (!b)
+		return (0);
+	for (; b[bin_len]; bin_len++)
+		;
+	for (bin_len -= 1; bin_len >= 0; bin_len--)
+	{
+		if (b[bin_len] != '0' && b[bin_len] != '1')
+			return (0);
+		new_int += result * (b[bin_len] - '0');
+		result *= 2;
+	}
+	return (new_int);
 }
