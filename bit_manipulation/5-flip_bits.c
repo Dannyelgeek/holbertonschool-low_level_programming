e7caddb8b77599c3959c71b0e679cb9dddfbc32e
Dannyelgeek <daot1993@gmail.com>
2022-12-02 19:05:59 -0500

    binary manipulation task 5

---
+++ bit_manipulation/5-flip_bits.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * flip_bits - returns the number of bits you would need to
+ * flip to get from one number to another.
+ * @n: number base
+ * @m: number to transform
+ * Return: the number of bits needed.
+ * 
+*/
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+    int ind, res = 0;
+
+    ind = 8 * sizeof(n) - 1;
+    while (ind >= 0)
+    {
+        if (((n ^ m) >> ind) & 1)
+            res += 1;
+        ind--;
+    }
+    return (res);
+}
