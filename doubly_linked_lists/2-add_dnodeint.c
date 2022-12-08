d1896553b7ac5faf31099a80b4f44a4cb9d64865
Dannyelgeek <daot1993@gmail.com>
2022-12-08 17:31:27 -0500

    D_linked lists task 2

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -11,7 +11,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *new_nod;
 
-	if(!head)
+	if (!head)
 		return (NULL);
 	new_nod = (dlistint_t *)malloc(sizeof(dlistint_t));
 	if (!new_nod)
