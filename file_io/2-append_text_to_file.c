68bcbfd16429a6c1f0987963da5bab3636f87de4
Dannyelgeek <daot1993@gmail.com>
2022-12-08 12:44:16 -0500

    File I/O task 2

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -22,11 +22,11 @@ int append_text_to_file(const char *filename, char *text_content)
 		return (1);
 	while (text_content[len])
 		len += 1;
-		
+
 	wr = write(op, text_content, len);
 
 	if (op == -1 || wr == -1)
 		return (-1);
-	close(op);
+	close(wr == -1 ? -1 : 1);
 	return (1);
 }
