d2af90dc924ad51cfd23c88104ee3698aa75be08
Dannyelgeek <daot1993@gmail.com>
2022-12-08 10:08:33 -0500

    File I/O task 0

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -10,25 +10,25 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    ssize_t op, re, wr;
-    char *buff;
+	ssize_t op, re, wr;
+	char *buff;
 
-    if(!filename)
-        return (0);
-    buff = malloc(sizeof(char) * letters);
-    if (!buff)
-        return (0);
+	if (!filename)
+		return (0);
+	buff = malloc(sizeof(char) * letters);
+	if (!buff)
+		return (0);
 
-    op = open(filename, O_RDONLY);
-    re = read(op, buff, letters);
-    wr = write(STDOUT_FILENO, buff, re);
+	op = open(filename, O_RDONLY);
+	re = read(op, buff, letters);
+	wr = write(STDOUT_FILENO, buff, re);
 
-    if (op == -1 || re == -1 || wr == -1 || wr != re)
-    {
-        free(buff);
-        return (0);
-    }
-    free(buff);
-    close(op);
-    return (wr);
+	if (op == -1 || re == -1 || wr == -1 || wr != re)
+	{
+		free(buff);
+		return (0);
+	}
+	free(buff);
+	close(op);
+	return (wr);
 }
