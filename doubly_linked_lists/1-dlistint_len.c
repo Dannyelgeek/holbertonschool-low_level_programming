c667236d2c9f092ff5242863725799bf91b206e3
Dannyelgeek <daot1993@gmail.com>
2022-12-08 17:08:40 -0500

    D_linked lists task 1

--- doubly_linked_lists/1-dlistint_len.c
+++ doubly_linked_lists/1-dlistint_len.c
@@ -8,12 +8,12 @@
 
 size_t dlistint_len(const dlistint_t *h)
 {
-    size_t num_nod = 0;
+	size_t num_nod = 0;
 
-    while (h)
-    {
-        h = h->next;
-        num_nod += 1;
-    }
-    return (num_nod);
+	while (h)
+	{
+		h = h->next;
+		num_nod += 1;
+	}
+	return (num_nod);
 }
