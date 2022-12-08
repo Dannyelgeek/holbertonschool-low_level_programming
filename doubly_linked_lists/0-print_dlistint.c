d181487e6138418bd5705412fcf21e42246409e5
Dannyelgeek <daot1993@gmail.com>
2022-12-08 16:52:44 -0500

    D_linked lists task 0

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -8,13 +8,13 @@
 
 size_t print_dlistint(const dlistint_t *h)
 {
-    size_t num_nod = 0;
+	size_t num_nod = 0;
 
-    while(h)
-    {
-        printf("%d\n", h->n);
-        h = h->next;
-        num_nod += 1;
-    }
-    return (num_nod);
+	while(h)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		num_nod += 1;
+	}
+	return (num_nod);
 }
