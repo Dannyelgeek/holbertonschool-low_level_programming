fd0513c7f140cae712b94e57ada5e40ab4927774
Dannyelgeek <daot1993@gmail.com>
2022-12-08 15:43:05 -0500

    File I/O task 3

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -3,36 +3,6 @@
 int closerr(int error, char *s, int file_d);
 
 /**
- * closerr - close the file with error.
- * @error: exit value.
- * @s: name of the files.
- * @file_d: file descriptor
- * Return: 0 if exited correctly.
-*/
-
-int closerr(int error, char *s, int file_d)
-{
-	switch (error)
-	{
-		case 97:
-			dprintf(2, "Usage: cp file_from file_to\n");
-			exit(error);
-		case 98:
-			dprintf(2, "Error: Can´t read from file %s\n", s);
-			exit(error);
-		case 99:
-			dprintf(2, "Error: Can´t write to %s\n", s);
-			exit(error);
-		case 100:
-			dprintf(2, "Error: can´t close fd %d\n", file_d);
-			exit(error);
-		default:
-			return (0);
-	}
-}
-
-
-/**
  * main - copies the content of a file to another file.
  * @argc: number of arguments
  * @argv: pointer to the argument array.
@@ -41,30 +11,51 @@ int closerr(int error, char *s, int file_d)
 
 int main(int argc, char *argv[])
 {
-	int file_a, file_b, re, wr;
-	char *buff[1024];
+	int file_a, file_b, wr;
+	ssize_t size = 1024;
+	char buff[1024];
 
 	if (argc != 3)
-		closerr(97, NULL, 0);
-
-	file_b = open(argv[2], O_CREAT | O_TRUNC | O_WRONLY, 0664);
-	if (file_b == -1)
-		closerr(99, argv[2], 0);
-
-	file_a = open(argv[1], O_RDONLY);
-	if (file_a == -1)
-		closerr(98, argv[1], 0);
-
-	while ((re = read(file_a, buff, 1024)) != 0)
 	{
-		if (re == -1)
-			closerr(98, argv[1], 0);
-
-		wr = write(file_b, buff, re);
-		if (wr == -1)
-			closerr(99, argv[2], 0);
+		dprintf(2, "Usage: cp file_from file_to\n");
+		exit(97);
 	}
-	close(file_b) == -1 ? (closerr(100, NULL, file_b)) : close(file_b);
-	close(file_a) == -1 ? (closerr(100, NULL, file_a)) : close(file_a);
+	file_a = open(argv[1], O_RDONLY);
+	if (file_a == -1);
+	{
+		dprintf(2, "Error: Can´t read from file %s\n", argv[1]);
+		exit(98);
+	}
+	file_b = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC | O_APPEND, 0664);
+	if (file_b == -1)
+	{
+		dprintf(2, "Error: Can´t write from file %s\n", argv[2]);
+		exit(99);
+	}
+	while (size == 1024)
+	{
+		size = read(file_a, buff, 1024);
+		if (size == -1)
+		{
+			dprintf(2, "Error: Can´t read from file %s\n", argv[1]);
+			exit(98);
+		}
+		wr = write(file_b, buff, size);
+		if (wr == -1)
+		{
+			dprintf(2, "Error: Can´t write to %s\n", argv[2]);
+			exit(99);
+		}
+	}
+	if (close(file_a) == -1)
+	{
+		dprinf(2, "Error: Can´t close fd %d\n", file_a);
+		exit(100);
+	}
+	if (close(file_b) == -1)
+	{
+		dprintf(2, "Error: Can´t close fd %d\n", file_b);
+		exit(100);
+	}
 	return (0);
 }
