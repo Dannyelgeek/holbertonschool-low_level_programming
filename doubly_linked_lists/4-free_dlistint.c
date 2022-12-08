d356e1eb5222987584da2bdf6a9d596b4ee2056f
Dannyelgeek <daot1993@gmail.com>
2022-12-08 18:25:20 -0500

    D_linked lists task 4

---
+++ doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_dlistint - frees a list.
+ * @head: pointer to the header of th list.
+*/
+
+void free_dlistint(dlistint_t *head)
+{
+    dlistint_t *tmp;
+
+    while (head)
+    {
+        tmp = head;
+        head = head->next;
+        tmp->prev = NULL;
+        tmp->next = NULL;
+        free(tmp);
+    }
+}
