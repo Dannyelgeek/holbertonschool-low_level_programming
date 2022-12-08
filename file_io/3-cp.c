3fa5501a44b7462abaa61311a78014d267bdac28
Dannyelgeek <daot1993@gmail.com>
2022-12-08 14:36:28 -0500

    File I/O task 3

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -10,7 +10,7 @@ int closerr(int error, char *s, int file_d);
 
 int main(int argc, char *argv[])
 {
-	int file_a, file_b, file_c, re, wr;
+	int file_a, file_b, re, wr;
 	char buff[1024];
 
 	if (argc != 3)
