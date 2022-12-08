f649763877796d31d46ec9a71728f9a8356f3c09
Dannyelgeek <daot1993@gmail.com>
2022-12-08 18:03:37 -0500

    D_linked lists task 2

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -4,7 +4,7 @@
  * add_dnodeint -  adds a new node at the beginning of a list.
  * @head: current location in the list.
  * @n: number tu add in the lists.
- * Return: *head if exited correctly.
+ * Return: new_nod if exited correctly.
 */
 
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
