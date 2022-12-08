034cfc3daa6747249c78347cd729c9a9fd2d58cc
Dannyelgeek <daot1993@gmail.com>
2022-12-08 15:12:18 -0500

    File I/O task 3

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -1,6 +1,37 @@
 #include "main.h"
 
 int closerr(int error, char *s, int file_d);
+
+/**
+ * closerr - close the file with error.
+ * @error: exit value.
+ * @s: name of the files.
+ * @file_d: file descriptor
+ * Return: 0 if exited correctly.
+*/
+
+int closerr(int error, char *s, int file_d)
+{
+	switch (error)
+	{
+		case 97:
+			dprintf(2, "Usage: cp file_from file_to\n");
+			exit(error);
+		case 98:
+			dprintf(2, "Error: Can´t read from file %s\n", s);
+			exit(error);
+		case 99:
+			dprintf(2, "Error: Can´t write to %s\n", s);
+			exit(error);
+		case 100:
+			dprintf(2, "Error: can´t close fd %d\n", file_d);
+			exit(error);
+		default:
+			return (0);
+	}
+}
+
+
 /**
  * main - copies the content of a file to another file.
  * @argc: number of arguments
@@ -36,33 +67,4 @@ int main(int argc, char *argv[])
 	close(file_b) == -1 ? (closerr(100, NULL, file_b)) : close(file_b);
 	close(file_a) == -1 ? (closerr(100, NULL, file_a)) : close(file_a);
 	return (0);
-}
-
-/**
- * closerr - close the file with error.
- * @error: exit value.
- * @s: name of the files.
- * @file_d: file descriptor
- * Return: 0 if exited correctly.
-*/
-
-int closerr(int error, char *s, int file_d)
-{
-	switch (error)
-	{
-		case 97:
-			dprintf(2, "Usage: cp file_from file_to\n");
-			exit(error);
-		case 98:
-			dprintf(2, "Error: Can´t read from file %s\n", s);
-			exit(error);
-		case 99:
-			dprintf(2, "Error: Can´t write to %s\n", s);
-			exit(error);
-		case 100:
-			dprintf(2, "Error: can´t close fd %d\n", file_d);
-			exit(error);
-		default:
-			return (0);
-	}
 }
