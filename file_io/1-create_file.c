dd54e2c16872ee7dea3fde3684267399287415da
Dannyelgeek <daot1993@gmail.com>
2022-12-08 12:29:07 -0500

    File I/O task 1

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -15,7 +15,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	if (!text_content)
 	{
-		for (; text_content[len]; len +=1)
+		for (len = 0; text_content[len]; len += 1)
 			;
 	}
 
