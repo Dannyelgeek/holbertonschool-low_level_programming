456555a859c5c4f7af2a39a169e45106967914b7
Dannyelgeek <daot1993@gmail.com>
2023-01-12 08:29:21 -0500

    search alg task 1

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -10,20 +10,24 @@
 
 int binary_search(int *array, size_t size, int value)
 {
-	size_t ind, left, right;
+	size_t ind = 0, left, right;
 
 	if (!array)
 		return (-1);
 
-	for (left = 0; right = size - 1; right >= left)
+
+	for (left = 0; right = size -1; right >= left)
 	{
 			printf("Searching in array: ");
 			for (ind = left; ind < right; ind++)
 				printf("%d, ", array[ind]);
+
 			printf("%d\n", array[ind]);
 
 			ind = left + (right - left) / 2;
 			if (array[ind] == value)
+				return (ind);
+			if (array[ind] > value)
 				right = ind - 1;
 			else
 				left = ind + 1;
