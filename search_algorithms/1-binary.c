d9526eb1fd4f565a9fa53d08ffe23206ae33f459
Dannyelgeek <daot1993@gmail.com>
2023-01-12 08:51:07 -0500

    search alg task 1

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -12,7 +12,7 @@ int binary_search(int *array, size_t size, int value)
 {
 	size_t ind = 0, left = 0, middle = 0, right = size - 1;
 
-	if (array == NULL)
+	if (!array)
 		return (-1);
 
 	while (left <= right)
